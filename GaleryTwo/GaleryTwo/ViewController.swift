b92deea8d98610edbdb2758772cbdbbbeba47d0c
Дрозд Денис <pavelzuravkov@MacBook-Pro.Dlink>
2023-10-12 20:38:43 +0300

    commit

---
+++ GaleryTwo/GaleryTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GaleryTwo
+//
+//  Created by Дрозд Денис on 12.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
