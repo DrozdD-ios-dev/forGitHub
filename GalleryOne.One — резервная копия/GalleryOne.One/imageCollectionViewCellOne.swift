4305f4c3f825afd33973dd16d1637b63cd32dc40
Дрозд Денис <pavelzuravkov@MacBook-Pro.local>
2023-10-15 11:31:59 +0300

    test

---
+++ GalleryOne.One — резервная копия/GalleryOne.One/imageCollectionViewCellOne.swift
@@ -0,0 +1,18 @@
+//
+//  imageCollectionViewCellOne.swift
+//  GalleryOne.One
+//
+//  Created by Дрозд Денис on 13.10.2023.
+//
+
+import UIKit
+
+class imageCollectionViewCellOne: UICollectionViewCell {
+    @IBOutlet weak var photoViewOne: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
